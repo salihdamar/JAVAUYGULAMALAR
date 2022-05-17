82b35dae2e798ee40528a2be495792fef5e784ea
Salih DAMAR <47279445+salihdamar@users.noreply.github.com>
2022-05-17 18:09:03 +0300

    Add files via upload

---
+++ HavaSicakligi/src/HavaSicakligi.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class HavaSicakligi {
+    public static void main(String[] args) {
+
+        /**
+         * Koşullar :
+         *  -Sıcaklık 5'dan küçük ise "Kayak" yapmayı öner.
+         *  -Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner.
+         *  -Sıcaklık 15 ve 25 arasında ise "Piknik" etkinliğini öner.
+         *  -Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.
+         *
+         * Ödev: Aynı örnek üzerinden if koşulları başka hangi şekilde oluşturulabilirdi farklı çözüm yolları bulunuz.
+         */
+        int sicaklik;
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Sıcaklık Değerini Giriniz(Derce Cinsinden)");
+        sicaklik=sc.nextInt();
+
+        /* İki parça halinde koşullar oluşturuldu
+
+        boolean kayak= sicaklik<5;
+        boolean sinema= 5<=sicaklik && sicaklik<15;
+
+        String sinemaveYuzme = sinema ? "Sinemaya Gidebilirsiniz":"Yüzmeye Gidebilirsiniz";
+        String durum = kayak ? "Kayak Yapabilirsiniz" : sinemaveYuzme;
+        */
+
+        // Tek parça halinde koşullar oluşturuldu
+        String durum= (sicaklik<5) ? "Kayak Yapabilirsiniz" : ( (5<=sicaklik && sicaklik<15) ? "Sinemaya Gidebilirsiniz":( (15<=sicaklik && sicaklik<25) ? "Pikniğe Gidebilirsiniz":"Yüzmeye Gidebilirsiniz"));
+
+        System.out.println(durum);
+    }
+}
