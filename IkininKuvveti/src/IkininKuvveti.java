983351e0ae64c8c0639aae7d7f32c73a020fd819
Salih DAMAR <47279445+salihdamar@users.noreply.github.com>
2022-05-25 23:12:41 +0300

    Add files via upload

---
+++ IkininKuvveti/src/IkininKuvveti.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class IkininKuvveti {
+    public static void main(String[] args) {
+        /**
+         *Java döngüler ile girilen sayıya kadar olan 2'nin kuvvetlerini ekrana yazdıran programı yazıyoruz.
+         */
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz");
+        int sayi=sc.nextInt();
+
+        System.out.println( sayi+ " sayısından küçük ikinin kuvvetleri ");
+        for (int i=1; i<=sayi; i*=2){
+            System.out.println(i);
+        }
+
+    }
+}
