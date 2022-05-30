dd9d5a4a3f2ad5d35d707433b284a79f9f9db63b
Salih DAMAR <47279445+salihdamar@users.noreply.github.com>
2022-05-31 02:27:16 +0300

    Add files via upload

---
+++ RecursiveDesen/src/RecursiveDesen.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class RecursiveDesen {
+    /** Java dilinde kullanıcıdan alınan n değerine göre aşağıdaki kurala uyan döngü kullanmadan bir
+     * "Recursive" metot yazın.
+     * Kural : Girilen sayı 0 veya negatif olduğu yere kadar girilen sayıdan 5 rakamını çıkarın. Her
+     * çıkarma işlemi sırasında ekrana son değeri yazdırın. Sayı negatif veya 0 olduktan sonra tekrar
+     * 5 ekleyin. Yine her ekleme işleminde sayının son değerini ekrana yazdırın.
+     *
+     * N Sayısı : 16
+     * Çıktısı : 16 11 6 1 -4 1 6 11 16
+     *
+     * N Sayısı : 25
+     * Çıktısı : 25 20 15 10 5 0 5 10 15 20 25
+     */
+
+    static int desenCikar(int sayi) {
+        if (sayi <= 0) return sayi;
+        System.out.print(sayi+" ");
+        return desenCikar(sayi -5);
+    }
+
+    static int desenTopla(int cikarSayi, int sayi ) {
+        if (cikarSayi > sayi) return cikarSayi;
+        System.out.print(cikarSayi + " ");
+        return desenTopla(cikarSayi + 5,sayi);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("N Sayisi:");
+        int sayi=sc.nextInt();
+        desenTopla(desenCikar(sayi),sayi);
+    }
+}
