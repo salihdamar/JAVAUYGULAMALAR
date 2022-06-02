ff11de4067dc84886f7786d275e0a5c4f2ae5e1b
Salih DAMAR <47279445+salihdamar@users.noreply.github.com>
2022-06-02 23:28:42 +0300

    Update Faktoriyel.java

--- Faktoriyel/src/Faktoriyel.java
+++ Faktoriyel/src/Faktoriyel.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Faktoriyel {
     public static void main(String[] args) {
+        
         /** Java ile faktöriyel hesaplayan program yazıyoruz.
          */
 
         Scanner sc=new Scanner(System.in);
         System.out.println("Faktöriyel Sayını Giriniz");
         int sayi=sc.nextInt();
-        int toplam=1;
-        int sayac=1;
+        
+        int toplam=1, sayac=1;
 
         while (sayac <= sayi){
             toplam*=sayac;
