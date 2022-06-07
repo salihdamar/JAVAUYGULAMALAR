59ff18a9b4431c98937f351a061f911306c81f27
Salih DAMAR <47279445+salihdamar@users.noreply.github.com>
2022-06-07 23:33:58 +0300

    Add files via upload

---
+++ CinZodyagi/src/CinZodyagi.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class CinZodyagi {
+    public static void main(String[] args) {
+        /** Java ile kullanıcıdan doğum tarihini alıp Çin Zodyağı değerini hesaplayan program yazınız.
+         *
+         * Çin Zodyağı nedir?:
+         * 4000 bin yıldır kullanılan bir astroloji çeşididir Çin astrolojisi ve insanları 12 değişik burç
+         * ve sembollerle tanımlar. Çin Zodyağı bu 12 burcun eşit aralıklarla(10 derece genişliğinde)
+         * sıralandığı bir hayvan halkasıdır ve yıldızlarla pek bir ilgisi yoktur.
+         *
+         * Çin Zodyağı nasıl hesaplanır?:
+         * Çin zodyağı hesaplanırken kişinin doğum yılının 12 ile bölümünde kalana göre bulunur.
+         *
+         * Doğum Tarihi %12 = 0 ➜ Maymun
+         * Doğum Tarihi %12 = 1 ➜ Horoz
+         * Doğum Tarihi %12 = 2 ➜ Köpek
+         * Doğum Tarihi %12 = 3 ➜ Domuz
+         * Doğum Tarihi %12 = 4 ➜ Fare
+         * Doğum Tarihi %12 = 5 ➜ Öküz
+         * Doğum Tarihi %12 = 6 ➜ Kaplan
+         * Doğum Tarihi %12 = 7 ➜ Tavşan
+         * Doğum Tarihi %12 = 8 ➜ Ejderha
+         * Doğum Tarihi %12 = 9 ➜ Yılan
+         * Doğum Tarihi %12 = 10 ➜ At
+         * Doğum Tarihi %12 = 11 ➜ Koyu
+         */
+
+        int dogumYili, kalan;
+        String mesaj="";
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Doğum Yılınızı Giriniz: ");
+        dogumYili = sc.nextInt();
+
+        kalan = dogumYili % 12;
+        switch (kalan){
+            case 0:
+                mesaj="Maymun";
+                break;
+            case 1:
+                mesaj="Horoz";
+                break;
+            case 2:
+                mesaj="Köpek";
+                break;
+            case 3:
+                mesaj="Domuz";
+                break;
+            case 4:
+                mesaj="Fare";
+                break;
+            case 5:
+                mesaj="Öküz";
+                break;
+            case 6:
+                mesaj="Kaplan";
+                break;
+            case 7:
+                mesaj="Tavşan";
+                break;
+            case 8:
+                mesaj="Ejderha";
+                break;
+            case 9:
+                mesaj="Yılan";
+                break;
+            case 10:
+                mesaj="At";
+                break;
+            case 11:
+                mesaj="Koyun";
+                break;
+            default:
+                System.out.println("Sıfırdan Büyük Bir değer Giriniz");
+        }
+        System.out.println("Çin Zodyağı Burcunuz: " +mesaj);
+
+
+    }
+}
